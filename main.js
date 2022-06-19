4bb2de289f363a0cda58e408e49fddb63846a6d0
Yasu829 <62891673+Yasu829@users.noreply.github.com>
2022-06-20 00:12:32 +0900

    hello.

---
+++ main.js
@@ -0,0 +1,6 @@
+let tweet;
+function play(){
+  let player = document.getElementById("player");
+  player.setAttribute("src","https://www.youtube.com/embed/NxJmIPIWf1I");
+  tweet = player.getAttribute("src");
+}
